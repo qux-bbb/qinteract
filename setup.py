718b96bfaf5214dbef58472dd4529f300ca41adb
qux-bbb <1147635419@qq.com>
2020-11-25 22:46:26 +0800

    update info

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setup(
     name='qinteract',
     version='0.1',
     author='qux-bbb',
+    author_email='1147635419@qq.com',
+    url='https://github.com/qux-bbb/qinteract',
     description='Process/Socket interactive',
     long_description=open('README.md', 'r', encoding='utf8').read(),
     long_description_content_type='text/markdown',
