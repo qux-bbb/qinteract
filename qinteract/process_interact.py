4bcd85c649a42b4b6ae8d8ff8be5b81f00a63454
qux-bbb <1147635419@qq.com>
2020-11-30 10:10:45 +0800

    add docstring

--- qinteract/process_interact.py
+++ qinteract/process_interact.py
@@ -8,23 +8,56 @@ from threading import Thread
 
 
 class ProcessInteraction:
+    """Interaction with a console process
+
+    Returns:
+        Instance: Instance of ProcessInteraction
+    """
     process = None
 
     def __init__(self, command):
+        """init
+
+        Args:
+            command (list): a command in the form of a list 
+        """
         self.process = subprocess.Popen(
             command, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 
     def __del__(self):
+        """Close process stdin/stdout
+        """
         self.process.stdin.close()
         self.process.stdout.close()
 
     def recvline(self):
+        """Get one line of content
+
+        Returns:
+            str: one line of content
+        """
         return self.process.stdout.readline().decode('utf8')
 
     def recv_n(self, n):
+        """Gets the content of the specified length
+
+        Args:
+            n (int): the specified length
+
+        Returns:
+            str: the content of the specified length
+        """
         return self.process.stdout.read(n).decode('utf8')
 
     def recvuntil(self, want_end_str):
+        """Get the content to the specific end
+
+        Args:
+            want_end_str (str): the specific end
+
+        Returns:
+            str: the content to the specific end
+        """
         current_str = ''
         while True:
             current_str += self.recv_n(1)
@@ -32,6 +65,14 @@ class ProcessInteraction:
                 return current_str
 
     def recvuntil_re(self, want_re_str):
+        """Get the content to the specific regex end
+
+        Args:
+            want_end_str (str): the specific regex end
+
+        Returns:
+            str: the content to the specific regex end
+        """
         current_str = ''
         while True:
             current_str += self.recv_n(1)
@@ -40,22 +81,38 @@ class ProcessInteraction:
                 return [current_str, s]
 
     def send(self, send_str):
+        """Send the content
+
+        Args:
+            send_str (str): the content want to send
+        """
         final_bytes = send_str.encode('utf8')
         self.process.stdin.write(final_bytes)
         self.process.stdin.flush()
 
     def sendline(self, send_str):
+        """Send the content and a linefeed
+
+        Args:
+            send_str (str): the content wat to send
+        """
         self.send(send_str+'\n')
     
     def interact(self):
+        """Auto get and wait input to send
+        """
         def recv_loop():
+            """Auto get content
+            """
             while True:
                 c = self.recv_n(1)
-                # print 写到控制台会有延时，直接用系统io写
+                # There will be a delay when "print" to the console. Write directly with system io
                 sys.stdout.write(c)
                 sys.stdout.flush()
 
         def send_loop():
+            """Wait input and send
+            """
             while True:
                 send_str = input()
                 self.sendline(send_str)
--- qinteract/socket_interact.py
+++ qinteract/socket_interact.py
@@ -8,22 +8,56 @@ from threading import Thread
 
 
 class SocketInteraction:
+    """Interaction with a socket
+
+    Returns:
+        Instance: Instance of SocketInteraction
+    """
     tcp_socket = None
 
     def __init__(self, host, port):
+        """init
+
+        Args:
+            host (str): a ip or domain
+            port (int): a port
+        """
         self.tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.tcp_socket.connect((host, port))
 
     def __del__(self):
+        """Close socket
+        """
         self.tcp_socket.close()
 
     def recvline(self):
+        """Get one line of content
+
+        Returns:
+            str: one line of content
+        """
         return self.recvuntil('\n')
     
     def recv_n(self, n):
+        """Gets the content of the specified length
+
+        Args:
+            n (int): the specified length
+
+        Returns:
+            str: the content of the specified length
+        """
         return self.tcp_socket.recv(n).decode('utf8')
 
     def recvuntil(self, want_end_str):
+        """Get the content to the specific end
+
+        Args:
+            want_end_str (str): the specific end
+
+        Returns:
+            str: the content to the specific end
+        """
         current_str = ''
         while True:
             current_str += self.recv_n(1)
@@ -31,6 +65,14 @@ class SocketInteraction:
                 return current_str
 
     def recvuntil_re(self, want_re_str):
+        """Get the content to the specific regex end
+
+        Args:
+            want_end_str (str): the specific regex end
+
+        Returns:
+            str: the content to the specific regex end
+        """
         current_str = ''
         while True:
             current_str += self.recv_n(1)
@@ -39,21 +81,37 @@ class SocketInteraction:
                 return [current_str, s]
 
     def send(self, send_str):
+        """Send the content
+
+        Args:
+            send_str (str): the content want to send
+        """
         final_bytes = send_str.encode('utf8')
         self.tcp_socket.send(final_bytes)
 
     def sendline(self, send_str):
+        """Send the content and a linefeed
+
+        Args:
+            send_str (str): the content wat to send
+        """
         self.send(send_str+'\n')
     
     def interact(self):
+        """Auto get and wait input to send
+        """
         def recv_loop():
+            """Auto get content
+            """
             while True:
                 c = self.recv_n(1)
-                # print 写到控制台会有延时，直接用系统io写
+                # There will be a delay when "print" to the console. Write directly with system io
                 sys.stdout.write(c)
                 sys.stdout.flush()
 
         def send_loop():
+            """Wait input and send
+            """
             while True:
                 send_str = input()
                 self.sendline(send_str)
